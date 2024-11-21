bc75040f0d8f6e0f80b37c5e1768eaac57f7339b
abhijit424515 <abhijit.amrendra.kumar@gmail.com>
2024-11-21 17:36:47 +0530

    added lpush

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ mod models;
 
 use dashmap::DashMap;
 use lazy_static::lazy_static;
-use models::{display, Primitive, Value};
+use models::{display, Container, Primitive, Value};
 use pest::{iterators::Pair, Parser};
 use pest_derive::Parser;
+use std::collections::VecDeque;
 use std::process::Command;
 use std::{fs, hash::Hash};
 
@@ -13,6 +14,24 @@ lazy_static! {
 }
 
 // ----------------------------------------------------------------
+
+pub fn deserialize(x: Pair<'_,Rule>) -> Primitive {
+    match x.as_rule() {
+        Rule::integer => {
+            let i = x.as_str().parse::<i64>().unwrap();
+            Primitive::Int(i)
+        }
+        Rule::float => {
+            let f = x.as_str().to_string();
+            Primitive::Flt(f)
+        }
+        Rule::str => {
+            let s = x.as_str().to_string();
+            Primitive::Str(s)
+        }
+        _ => unreachable!(),
+    }
+}
 
 fn clear_terminal() {
     if cfg!(target_os = "windows") {
@@ -42,7 +61,7 @@ fn actions(pair: Pair<'_, Rule>) {
         Rule::get => {
             let key = pair.into_inner().next().unwrap().as_str();
             match DB.get(key) {
-                Some(value) => Some(value.clone()),
+                Some(value) => Some(value.clone()), // [TODO] get shouldn't work for containers
                 None => Some(Value::Primitive(Primitive::Nil)),
             }
         }
@@ -51,35 +70,51 @@ fn actions(pair: Pair<'_, Rule>) {
             let k = z.next().unwrap().as_str();
 
             let v = z.next().unwrap();
-            let p = match v.as_rule() {
-                Rule::integer => {
-                    let i = v.as_str().parse::<i64>().unwrap();
-                    Primitive::Int(i)
-                }
-                Rule::float => {
-                    let f = v.as_str().to_string();
-                    Primitive::Flt(f)
-                }
-                Rule::str => {
-                    let s = v.as_str().to_string();
-                    Primitive::Str(s)
-                }
-                _ => unreachable!(),
-            };
+            let p = deserialize(v);
 
             DB.insert(k.to_string(), Value::Primitive(p));
             Some(Value::Primitive(Primitive::Ok))
         }
         Rule::del => {
             let mut count = 0;
-
-            let keys = pair.into_inner().map(|x| x.as_str()).collect::<Vec<_>>();
-            keys.into_iter().for_each(|key| match DB.remove(key) {
-                Some(_) => count += 1,
-                None => (),
+            pair.into_inner().for_each(|x| {
+                match DB.remove(x.as_str()) {
+                    Some(_) => count += 1,
+                    None => (),
+                }
             });
 
             Some(Value::Primitive(Primitive::Int(count)))
+        }
+        Rule::lpush => {
+            let mut z = pair.into_inner();
+            let k = z.next().unwrap().as_str();
+
+            if !DB.contains_key(k) {
+                DB.insert(
+                    k.to_string(),
+                    Value::Container(Container::List(VecDeque::new())),
+                );
+            };
+
+            let mut val = DB.get_mut(k).unwrap();
+            let val = val.value_mut();
+
+            match val {
+                Value::Container(Container::List(l)) => {
+                    z.for_each(|x| {
+                        let v = deserialize(x);
+                        l.push_front(v);
+                    });
+                }
+                _ => unreachable!(), // [TODO] handle error
+            };
+
+            let sz = match val {
+                Value::Container(Container::List(l)) => l.len() as i64,
+                _ => 0,
+            }; 
+            Some(Value::Primitive(Primitive::Int(sz)))
         }
         Rule::clear => {
             clear_terminal();
